9409ade9c2c9e2a9870f4f10d66ad20500e29cb1
chris-cloudreach <93588048+chris-cloudreach@users.noreply.github.com>
2023-07-03 12:43:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
 
+// THIS IS A COMMENT
+
 app.get('/', (req, res) => res.send('Hello Argo CD!'))
 app.listen(3000, () => console.log('Server ready'))
