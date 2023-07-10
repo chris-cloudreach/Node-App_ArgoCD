9f086fa553a49aa3a3738afe734c2c1ff07d9edf
chris-cloudreach <93588048+chris-cloudreach@users.noreply.github.com>
2023-07-10 12:59:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello chrisjr!'))
-app.listen(3000, () => console.log('Server ready'))
+app.get('/', (req, res) => res.send('Hello CHIKA!'))
+app.listen(3000, () => console.log('Server is not ready'))
