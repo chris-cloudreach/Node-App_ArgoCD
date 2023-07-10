73c576b3ec23ffd7894fd1e860de464e6d9e9cb9
Christopher <christopher.ogbunuzor@cloudreach.com>
2023-07-10 13:16:06 +0100

    creqte comflict

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello CHIKA!'))
-app.listen(3000, () => console.log('Server is not ready'))
+app.listen(6000, () => console.log('Server is not ready'))
