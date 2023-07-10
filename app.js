a320856bb89fb0755b15c4612979adbb821138a0
Christopher <christopher.ogbunuzor@cloudreach.com>
2023-07-10 13:01:27 +0100

    create merge conflict

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello chrisjr!'))
-app.listen(3000, () => console.log('Server ready'))
+app.get('/', (req, res) => res.send('Hello chrisjr2!'))
+app.listen(3000, () => console.log('Server YES ready'))
